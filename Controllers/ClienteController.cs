a0c5c015460249164088167b662a068b63524f29
idlcharles <charles.idlsc@gmail.com>
2022-10-27 20:14:15 -0300

    Inicio projeto asp net

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebLayer.Models;
+
+namespace WebLayer.Controllers
+{
+    public class ClienteController : Controller
+    {   public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ClienteInsertViewModel viewModel)
+        {
+            bool isUnique = false;
+            if (!isUnique)
+            {
+                ModelState.AddModelError("CPF", "CPF já esta em uso." +
+                    "");
+            }
+            return View();
+
+        }
+    }
+}
---
+++ Models/ClienteInsertViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebLayer.Models
+{
+    public class ClienteInsertViewModel
+    {        [Required(ErrorMessage = "Nome deve ser Informado.")]
+        public string Nome { get; set; }
+                [Required(ErrorMessage = "CPF deve ser Informado.")]
+        public string CPF { get; set; }
+                [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Data deve ser Informada.")]
+        public DateTime DataNascimento { get; set; }
+                [EmailAddress(ErrorMessage = "Email Inválido")]
+        [Required(ErrorMessage = "Email válido deve ser Informado.")]
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
